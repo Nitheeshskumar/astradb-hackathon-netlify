20f3006e75ec16c9ec51c7403ecfe38a5441e483
Nitheesh S Kumar <40543713+Nitheeshskumar@users.noreply.github.com>
2021-09-04 13:16:34 +0530

    Update UserServices.js

--- src/Services/UserServices.js
+++ src/Services/UserServices.js
@@ -1,26 +1,26 @@
 import axios from 'axios'
 
-export const createUser = async (body) => {
-    const response = await axios.post("/.netlify/functions/createUser", body);
+export const createUser = async (todo) => {
+    const response = await axios.post("/.netlify/functions/createUser", todo);
     return response.data
   };
 
- export const updateUser = async (body) => {
-    const response = await axios.post("/.netlify/functions/updateUser", body);
+ export const updateUser = async (todo) => {
+    const response = await axios.post("/.netlify/functions/updateUser", todo);
     return response.data
   };
 
-  export const authenticate = async (body) => {
-    const response = await axios.post("/.netlify/functions/authenticate", body);
+  export const authenticate = async (todo) => {
+    const response = await axios.post("/.netlify/functions/authenticate", todo);
     return response.data
   };
-  export const getProviders = async (body) => {
-    const response = await axios.post("/.netlify/functions/getProviders", body);
+  export const getTodos = async (todo) => {
+    const response = await axios.post("/.netlify/functions/getTodos", todo);
     return response.data
   };
 
-  export const createProvider = async (body) => {
-    const response = await axios.post("/.netlify/functions/createProvider", body);
+  export const createTodos = async (todo) => {
+    const response = await axios.post("/.netlify/functions/createTodo", todo);
     return response.data
   };
 
