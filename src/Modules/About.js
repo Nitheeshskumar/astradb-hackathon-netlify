2a25cc060e2daa50e26aba324781bb83734a986f
Nitheesh S Kumar <40543713+Nitheeshskumar@users.noreply.github.com>
2021-09-04 13:00:45 +0530

    Create About.js

---
+++ src/Modules/About.js
@@ -0,0 +1,16 @@
+import linkedin from '../Assets/linkedin-logo.png'
+const About=()=>{
+    return  <div className="about">
+    <div className="container">
+
+ <div className="modal-content text-center margin0">
+        <div className="modal-header">
+          <h4 className="modal-title margin0">
+          Reach Me Out At  </h4> </div>
+      
+      </div>
+    </div>
+  </div>
+}
+
+export default About
